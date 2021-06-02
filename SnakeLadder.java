664f92b024fbdd246d698499dbcda75a11f5e01f
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-01 21:50:17 -0400

    add : SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,6 +6,14 @@ public class SnakeLadder
 	{
 
 		System.out.println("Welcome to Snake And Ladder Program");
-	}
-
+		{
+			int random_check = (int)Math.floor(Math.random() * 10) % 7;
+				
+					if(random_check != 0)
+					{
+						System.out.println("The player dice number is : " + random_check);
+					}
+				}
+			}
 }
+		
