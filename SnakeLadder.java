4693d2d0481c110890bc4f521a499cd5aa87f864
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-02 20:34:30 -0400

    add : SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,13 +7,48 @@ public class SnakeLadder
 
 		System.out.println("Welcome to Snake And Ladder Program");
 		{
-			int single_player = 0;
-			System.out.println("Game Starts at: " + single_player);
-			int random = (int)Math.floor(Math.random() * 10) & 6;
-			if(random != 0)
+	   		int random_number = 0;
+			int first_Ladder_Position = 10;
+			int jumpFrom_firstLadder_Position = 36;
+			int second_Ladder_Position = 45;
+			int jumpFrom_secondLadder_Position = 89;
+			int diff1, diff2;
+			int firstSnake_Position = 20;
+			int secondSnake_Position = 68;
+			while(random_number < 100)
 			{
-				single_player = single_player + random ;
-				System.out.println("player moved to " + single_player);
+				int random_Check = (int)Math.floor(Math.random() * 10) % 6;
+				if(random_Check != 0)
+				{
+					random_number = random_number + random_Check;
+				}
+				if(random_number == firstSnake_Position)
+				{
+				 	System.out.println("if Ladder Moved to Snakes Position and moved towards backward direction:");
+					random_number = random_number - random_Check;
+				}
+				else if(random_number == secondSnake_Position)
+				{
+					System.out.println("if Ladder Moved to Snakes Position and moved towards backward direction:");
+					random_number = random_number - random_Check;
+				}
+				if(random_number == first_Ladder_Position)
+				{
+					System.out.println("Ladder moved to upper Ladder Position is:");
+					diff1 = jumpFrom_firstLadder_Position - first_Ladder_Position;
+					random_number = random_number + diff1;
+				}
+				if(random_number == first_Ladder_Position)
+	                        {
+	                                System.out.println("Ladder moved to upper Ladder Position is:");
+	                                diff2 = jumpFrom_secondLadder_Position - second_Ladder_Position;
+	                                random_number = random_number + diff2;
+	                        }
+
+				if(random_Check != 0)
+				{
+					System.out.println("Ladder got a Number : "+ random_Check + " My Ladder Moved to the Position : " + random_number);
+				}
 			}
 		}
 	}
