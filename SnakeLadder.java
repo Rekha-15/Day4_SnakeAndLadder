e9dbd8350e3c7f7b5ea0569c76126e05a15a9d5f
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-02 20:49:23 -0400

    add : SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,12 +7,7 @@ public class SnakeLadder
 
 		System.out.println("Welcome to Snake And Ladder Program");
 		{
-	   		int random_number = 0;
-			int first_Ladder_Position = 10;
-			int jumpFrom_firstLadder_Position = 36;
-			int second_Ladder_Position = 45;
-			int jumpFrom_secondLadder_Position = 89;
-			int diff1, diff2;
+			int random_number = 0;
 			int firstSnake_Position = 20;
 			int secondSnake_Position = 68;
 			while(random_number < 100)
@@ -24,27 +19,14 @@ public class SnakeLadder
 				}
 				if(random_number == firstSnake_Position)
 				{
-				 	System.out.println("if Ladder Moved to Snakes Position and moved towards backward direction:");
-					random_number = random_number - random_Check;
+				 	System.out.println("Dies Moved to Snakes Position and moved towards backward direction:");
+					random_number = 0;
 				}
 				else if(random_number == secondSnake_Position)
 				{
-					System.out.println("if Ladder Moved to Snakes Position and moved towards backward direction:");
-					random_number = random_number - random_Check;
+					System.out.println("Dies Moved to Snakes Position and moved towards backward direction:");
+					random_number = 0;
 				}
-				if(random_number == first_Ladder_Position)
-				{
-					System.out.println("Ladder moved to upper Ladder Position is:");
-					diff1 = jumpFrom_firstLadder_Position - first_Ladder_Position;
-					random_number = random_number + diff1;
-				}
-				if(random_number == first_Ladder_Position)
-	                        {
-	                                System.out.println("Ladder moved to upper Ladder Position is:");
-	                                diff2 = jumpFrom_secondLadder_Position - second_Ladder_Position;
-	                                random_number = random_number + diff2;
-	                        }
-
 				if(random_Check != 0)
 				{
 					System.out.println("Ladder got a Number : "+ random_Check + " My Ladder Moved to the Position : " + random_number);
